f7b8e5166fc94721c8b573857fd53fe183ae4b57
Dino3K <barragan5907@gmail.com>
2024-09-16 16:27:12 -0500

    ahi va todo

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Inicio</h2>
+	<p>Bienvenidos a la pagina de Dino3K :D</p>
+</div>
+
+<?=template_footer()?>
